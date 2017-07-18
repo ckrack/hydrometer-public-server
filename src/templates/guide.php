cf41b16b6b921a712c6401497fc3c73ac6b740aa
Clemens Krack <ckrack@gmail.com>
2017-07-19 01:06:55 +0200

    Add bullshit templates

---
+++ src/templates/guide.php
@@ -0,0 +1,18 @@
+<?php $this->layout('layouts/index.php', ['user' => (isset($user) ? $user : null)]) ?>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3">
+        Guide
+    </h1>
+    <p class="lead">
+        You own an <a href="">iSpindel</a> electronic hydrometer?<br>
+        Monitor your fermentations in an easy to use online interface!
+    </p>
+    <hr class="my-4">
+    <p>
+        Best of all, it's free.
+    </p>
+    <a class="btn btn-primary btn-lg ml-auto mr-0" href="/auth/register" role="button">Sign up</a>
+  </div>
+</div>
