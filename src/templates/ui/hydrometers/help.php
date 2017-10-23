defeed248d4158914e31944682cf087446a231c5
Clemens Krack <ckrack@gmail.com>
2017-10-23 16:24:02 +0200

    Update setup guide

--- src/templates/ui/hydrometers/help.php
+++ src/templates/ui/hydrometers/help.php
@@ -8,7 +8,14 @@
 <!-- Nav tabs -->
 <ul class="nav nav-tabs" role="tablist">
   <li class="nav-item">
-    <a class="nav-link active" data-toggle="tab" href="#ispindle" role="tab"><?=_('iSpindle')?></a>
+    <a class="nav-link active" data-toggle="tab" href="#intro" role="tab"><?=_('Intro')?></a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><?=_('iSpindle')?></a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" data-toggle="tab" role="tab" href="#ispindle-tcp" aria-expanded="true" aria-controls="#ispindle-tcp"><?=_('TCP')?></a>
+          <a class="dropdown-item" data-toggle="tab" role="tab" href="#ispindle-http" aria-expanded="true" aria-controls="#ispindle-http"><?=_('HTTP')?></a>
+        </div>
   </li>
   <li class="nav-item">
     <a class="nav-link" data-toggle="tab" href="#tilt" role="tab"><?=_('Tilt')?></a>
@@ -23,7 +30,45 @@
 
 <!-- Tab panes -->
 <div class="tab-content">
-  <div class="tab-pane active" id="ispindle" role="tabpanel">
+  <div class="tab-pane active" id="intro" role="tabpanel">
+    <div class="card">
+        <div class="card-header">
+            <?=_('Setup')?>
+        </div>
+        <div class="card-block">
+            Please select your device-type to get setup instructions.
+        </div>
+    </div>
+  </div>
+  <div class="tab-pane" id="ispindle-tcp" role="tabpanel">
+    <div class="card">
+        <div class="card-header">
+            <?=_('iSpindle TCP')?>
+        </div>
+        <div class="card-block">
+            <ol>
+                <li class="card-text"><?=_('Put iSpindle into configuration mode by pressing reset')?></li>
+                <li class="card-text"><?=_('Open the configuration page and put in:')?></li>
+                <li class="card-text">
+                    <dl>
+                        <dt><?=_('Service type')?></dt>
+                        <dd><?=_('TCP')?></dd>
+
+                        <dt><?=_('Server address')?></dt>
+                        <dd><?=getenv('TCP_API_HOST')?></dd>
+
+                        <dt><?=_('Server port')?></dt>
+                        <dd><?=getenv('TCP_API_PORT')?></dd>
+
+                        <dt><?=_('Token')?></dt>
+                        <dd><?=$token->getValue()?></dd>
+                    </dl>
+                </li>
+            </ol>
+        </div>
+    </div>
+  </div>
+  <div class="tab-pane" id="ispindle-http" role="tabpanel">
     <div class="card">
         <div class="card-header">
             <?=_('iSpindle HTTP')?>
@@ -57,7 +102,6 @@
               <ol>
                 <li class="card-text"><?=_('Open TILT app on your phone and enter settings.')?></li>
                 <li class="card-text"><?=_('Choose color of your tilt')?></li>
-                <li class="card-text"><?=_('Choose color of your tilt')?></li>
                 <li class="card-text">
                     <dl>
                         <dt><?=_('Logging URL')?></dt>
@@ -68,8 +112,26 @@
         </div>
       </div>
   </div>
-  <div class="tab-pane" id="plaato" role="tabpanel">...</div>
-  <div class="tab-pane" id="beerbug" role="tabpanel">...</div>
+  <div class="tab-pane" id="plaato" role="tabpanel">
+      <div class="card">
+          <div class="card-header">
+              <?=_('Plaato')?>
+          </div>
+          <div class="card-block">
+              Apparently the Plaato device is not yet supported.
+          </div>
+      </div>
+  </div>
+  <div class="tab-pane" id="beerbug" role="tabpanel">
+    <div class="card">
+        <div class="card-header">
+            <?=_('Beerbug')?>
+        </div>
+        <div class="card-block">
+            Apparently the Beerbug device is not yet supported.
+        </div>
+    </div>
+  </div>
 </div>
 
 
