021774d39e0a842a88b5518238be53337700e4d9
Clemens Krack <ckrack@gmail.com>
2017-10-23 16:22:17 +0200

    Add configuration option for db port

--- src/settings.php
+++ src/settings.php
@@ -71,7 +71,7 @@ return [
                 'password' => getenv('DB_PASS'),
                 'charset'  => 'utf8mb4',
                 'collation' => 'utf8mb4_unicode_ci',
-                'port' => 3306,
+                'port' => getenv('DB_PORT'),
             ],
         ],
         'languages' => [
