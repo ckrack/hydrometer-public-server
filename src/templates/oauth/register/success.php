67ce26f36841466f341763b6652d5208ad3fb5ec
Clemens Krack <ckrack@gmail.com>
2017-10-28 23:35:33 +0200

    Add OAuth controller and templates

---
+++ src/templates/oauth/register/success.php
@@ -0,0 +1,9 @@
+<?php $this->layout('layouts/index.php', ['user' => (isset($user) ? $user : null)]) ?>
+
+<div class="alert alert-success mt-4" role="alert">
+  <h4 class="alert-heading">Well done!</h4>
+  <p>
+      Yay, welcome on board <?=$this->e($user->getUsername())?>!
+  </p>
+</div>
+<a href="/ui/" class="btn btn-primary btn-lg">Go to your hydrometers</a>
