6e9cc3361b2469b1853dc102af4729637c4f528f
Clemens Krack <info@clemenskrack.com>
2017-11-12 19:58:06 +0100

    Decrease font size on mobile

--- src/templates/layouts/index.php
+++ src/templates/layouts/index.php
@@ -15,6 +15,7 @@
 
     <link rel="stylesheet" href="/css/c3.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/main.css">
 
 
     <link rel="author" href="humans.txt">
