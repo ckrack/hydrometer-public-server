6713e6f8869a1c8e88d73614ba659e0bafec9dcb
Clemens Krack <info@clemenskrack.com>
2017-12-08 16:42:58 +0100

    Add formatter options for rounding and treshold for charts

--- src/Modules/Formula/Formatter.php
+++ src/Modules/Formula/Formatter.php
@@ -25,6 +25,9 @@ class Formatter
             case "SG":
                 return number_format($value, 3, ".", ".")." SG";
 
+            case "°P":
+                return number_format($value, 2)." °P";
+
             case "°":
                 return number_format($value, 2)." °";
 
@@ -36,6 +39,53 @@ class Formatter
 
             default:
                 return number_format($value, 2)." ".$metric;
+                break;
+        }
+    }
+
+    public static function roundTo($metric = null)
+    {
+        switch ($metric) {
+            case "V":
+                return 2;
+
+            case "SG":
+                return 3;
+
+            case "°":
+                return 2;
+
+            case "°C":
+                return 2;
+
+            case "°F":
+                return 0;
+
+            default:
+                return 2;
+                break;
+        }
+    }
+
+    public static function treshold($metric = null)
+    {
+        switch ($metric) {
+            case "V":
+                return [2.7, 3.1, 3.5, 4.5];
+
+            case "°C":
+                return [-2.5, 8, 14, 22, 26, 35];
+
+            case "°F":
+                return [27, 46, 57, 72, 79, 100];
+
+            case "SG":
+                return [1.00, 1.03, 1.06, 1.1, 1.15, 1.18];
+
+            case "°P":
+                return [0, 5, 10, 15, 20, 40];
+
+            default:
                 break;
         }
     }
