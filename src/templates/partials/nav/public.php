bbfd6f4fd348092fe19a95e1c26666f885fce44b
Clemens Krack <info@clemenskrack.com>
2018-08-21 17:32:17 +0200

    Delete old templates and slim3 files

--- src/templates/partials/nav/public.php
+++
@@ -1,3 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand mb-0" href="/"><?=getenv('SITE_TITLE')?></a>
-</nav>
--- src/templates/ui/exception.php
+++
@@ -1,9 +0,0 @@
-<?php $this->layout('layouts/index.php', ['user' => (isset($user) ? $user : null)]) ?>
-
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <p class="lead">
-        <?=_("Sorry, an error occured during your request.")?>
-    </p>
-  </div>
-</div>
