5a4da92d6493895429d0fb718992e5b5cfef1046
Clemens Krack <info@clemenskrack.com>
2020-01-26 11:52:15 +0100

    feat(remoteConfig): return JSON with interval

--- src/Command/IspindelTcpServerCommand.php
+++ src/Command/IspindelTcpServerCommand.php
@@ -79,31 +79,25 @@ class IspindelTcpServerCommand extends Command
                         $this->logger->info('Empty?', ['' === $tcpInput]);
                         $jsonRaw .= $tcpInput;
 
-                        if ('' === trim($tcpInput)) {
+                        // get input until json closing bracket
+                        if (mb_strpos(trim($tcpInput), '}') ==! false) {
+                            $this->logger->info('Breaking');
                             break;
                         }
                     }
 
                     $this->logger->info('Compiled input: '.$jsonRaw);
 
-                    // close connection to client
-                    stream_socket_shutdown($client, STREAM_SHUT_RDWR);
-
-                    $this->logger->info('Closed client');
-
-                    fclose($client);
-
                     // now handle data
                     if (!$this->tcp->validateInput($jsonRaw)) {
                         $this->logger->error('Invalid input', [$jsonRaw]);
+                        continue;
                     }
 
                     $jsonDecoded = json_decode($jsonRaw, true);
 
                     if ((!is_array($jsonDecoded) && !is_object($jsonDecoded)) || json_last_error()) {
-                        // data not ok
                         $this->logger->info('Spindle data not ok', [$jsonDecoded, json_last_error()]);
-
                         continue;
                     }
 
@@ -116,10 +110,18 @@ class IspindelTcpServerCommand extends Command
                     $authData = $this->tokenAuth->authenticate($jsonDecoded['token']);
                     $this->tcp->saveData($jsonDecoded, $authData['hydrometer_id'], $authData['fermentation_id']);
 
+                    // write new or current interval
+                    fwrite($client, json_encode((object) ['interval' => $authData['interval'] ?? $jsonDecoded['interval']]));
+
+                    // close connection to client
+                    stream_socket_shutdown($client, STREAM_SHUT_RDWR);
+                    fclose($client);
+                    $this->logger->info('Closed client');
+
                     // sleep the database connection
                     $this->tcp->sleepDb();
                 } catch (\Exception $e) {
-                    $this->logger->error('Exception: '.$e->getMessage());
+                    $this->logger->error('Exception: '.$e->getMessage().$e->getFile().$e->getLine());
                 }
                 $this->logger->info('server done', [$input->getOption('port')]);
             }
